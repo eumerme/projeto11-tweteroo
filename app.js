00b042e8f53062e9c1fd8c8886d9761a618fb6a7
eumerme <euthaisetau@gmail.com>
2022-08-26 15:44:28 -0300

    feat: post sign-up

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let user = {};
+
+app.post('/sign-up', (req, res) => {
+    const userInfo = req.body;
+    user = { ...userInfo };
+    res.send(user);
+});
+
 app.listen(5000, () => console.log('Listening on 5000'));
